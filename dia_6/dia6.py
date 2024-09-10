a2d7d5865074715262bc907e5a393427d6266fdc
CauanLagrotta <caun06lagrotta@gmail.com>
2024-09-09 21:13:03 -0300

    Adicionando os dias 6 e 7

--- dia_6/dia6.py
+++ dia_6/dia6.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 # Cores
 preto = "#000000" 
@@ -14,30 +15,29 @@ def Bordas():
     canvas.create_line(0, 2, 180, 2, fill=branco, width=2)
     canvas.pack(pady=3)
 
-def par_ou_impar():
+def Calcular():
     try:
         n1 = int(entry_n1.get())
-
-        if n1 % 2 == 0:
-            resultado.config(text = 'Par')
-        else:
-            resultado.config(text = 'Impar')
-    
+        fatorial = 1
+        
+        for i in range(1, n1+1):
+            fatorial *= i
+            resultado.config(text = f'Fatorial: {fatorial}')
+        
     except ValueError:
         resultado.config(text='Insira apenas números válidos',  fg=vermelho)
 
 # Criando a janela
 janela = Tk()
-janela.title("Par ou Impar?")
+janela.title("Calculadora Fatorial")
 janela.geometry("350x340")
 janela.config(bg=cinza_escuro)
 
-
 # Titulo
-label_titulo = Label(janela, text="Par ou Impar?", font=('Arial bold', 20), background=cinza_escuro, fg=roxo)
+label_titulo = Label(janela, text="Calculadora Fatorial", font=('Arial bold', 20), background=cinza_escuro, fg=roxo)
 label_titulo.pack(pady=20)
 
-# Criando os frames
+# Criando Frames
 #Numero
 label_n1 = Label(janela, text="Insira um número: ", font=('Arial', 12), background=cinza_escuro, fg=branco)
 label_n1.pack(pady=10)
@@ -47,12 +47,20 @@ entry_n1.pack(pady=5)
 
 Bordas()
 
-botaoCalcular = Button(janela, text="Calcular", command=par_ou_impar, font=('Arial', 12), relief=FLAT, width=15, background=roxo, fg=branco, border=1 )
-botaoCalcular.pack(pady=15)
+botaoCalcular = Button(janela, text="Calcular", command=Calcular, font=('Arial', 12), relief=FLAT, width=15, background=roxo, fg=branco, border=1 )
+botaoCalcular = botaoCalcular.pack(pady=15)
 
 resultado = Label(janela, text="Resultado: ", font=('Arial bold', 18), background=cinza_escuro, fg=roxo)
 resultado.pack(pady=10)
 
 
-
 janela.mainloop()
+
+# n = int(input('Insira um número: '))
+
+# fatorial = 1
+
+# for i in range(1, n + 1):
+#     fatorial *= i
+
+# print(fatorial)
--- dia_7/dia7.py
+++ dia_7/dia7.py
@@ -13,32 +13,37 @@ def Bordas():
     canvas = Canvas(janela, width=180, height=2, background=cinza_escuro, highlightthickness=0)
     canvas.create_line(0, 2, 180, 2, fill=branco, width=2)
     canvas.pack(pady=3)
-
-def par_ou_impar():
-    try:
-        n1 = int(entry_n1.get())
-
-        if n1 % 2 == 0:
-            resultado.config(text = 'Par')
-        else:
-            resultado.config(text = 'Impar')
     
+    
+def Calcular():
+    try:
+        n = int(entry_n1.get())
+        i = 0
+        resultado_text = ""
+
+        while(i < 10):
+            i += 1
+            resultado_text += f'{n} x {i} = {n*i}\n'
+            
+        resultado.config(text=resultado_text)
+        
     except ValueError:
         resultado.config(text='Insira apenas números válidos',  fg=vermelho)
-
-# Criando a janela
+        
+        
+#Criando a janela
 janela = Tk()
-janela.title("Par ou Impar?")
-janela.geometry("350x340")
+janela.title("Calculando Tabuada")
+janela.geometry("500x550")
 janela.config(bg=cinza_escuro)
 
-
 # Titulo
-label_titulo = Label(janela, text="Par ou Impar?", font=('Arial bold', 20), background=cinza_escuro, fg=roxo)
+label_titulo = Label(janela, text="Calculando Tabuada", font=('Arial bold', 20), background=cinza_escuro, fg=roxo)
 label_titulo.pack(pady=20)
 
-# Criando os frames
+# Criando os frames 
 #Numero
+
 label_n1 = Label(janela, text="Insira um número: ", font=('Arial', 12), background=cinza_escuro, fg=branco)
 label_n1.pack(pady=10)
 
@@ -47,12 +52,22 @@ entry_n1.pack(pady=5)
 
 Bordas()
 
-botaoCalcular = Button(janela, text="Calcular", command=par_ou_impar, font=('Arial', 12), relief=FLAT, width=15, background=roxo, fg=branco, border=1 )
+botaoCalcular = Button(janela, text="Calcular", command=Calcular, font=('Arial', 12), relief=FLAT, width=15, background=roxo, fg=branco, border=1 )
 botaoCalcular.pack(pady=15)
 
-resultado = Label(janela, text="Resultado: ", font=('Arial bold', 18), background=cinza_escuro, fg=roxo)
+resultado = Label(janela, text=" ", font=('Arial bold', 18), background=cinza_escuro, fg=roxo)
 resultado.pack(pady=10)
 
 
 
+
 janela.mainloop()
+
+
+# n = int(input('Digite um número: '))
+# i = 0
+
+# while(i < 10):
+#     i += 1
+#     print(f'{n} x {i} = {n*i}')
+   
